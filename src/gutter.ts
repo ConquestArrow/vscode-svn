3caa7080248a76a7b162fcbdbcfd02d05e344624
ConquestArrow <ca@conquestarrow.com>
2016-01-31 02:24:16 +0900

    improve performance

--- src/gutter.ts
+++ src/gutter.ts
@@ -1,7 +1,10 @@
 import * as vscode from "vscode";
 import * as child from "child_process";
+import * as fs from "fs";
+import * as JsDiff from "diff";
 
 const diffParse = require("diff-parse")
+
 
 import * as event from "./event";
 import * as cmd from "./command";
@@ -9,21 +12,64 @@ import * as ui from "./ui";
 
 const w = vscode.window;
 const ws = vscode.workspace;
-const ae = w.activeTextEditor;
+let ae = w.activeTextEditor;
+
 type ExtContext = vscode.ExtensionContext
-type VSCodeChageEvent = vscode.TextEditor | vscode.TextDocumentChangeEvent;
+type VSCodeChangeEvent = vscode.TextEditor | vscode.TextDocumentChangeEvent;
 type MarkedLine = { add: boolean, del: boolean, contentLength?: number }[];
 
+type ClassifiedLine = {
+	decoType:DecoType;
+	value:string;
+	lineNumber:number;
+}
+
+interface IParsedDiff{
+	lines: DiffLine[];
+}
+interface DiffLine{
+	type: string,
+	ln: number, 
+	content: string 
+}
+interface TempLine{
+	ln:number,
+	added?:boolean,
+	removed?:boolean,
+	content:string
+}
+
+enum DecoType{
+	MODIFIED,
+	ADDED,
+	DELETED
+}
+
+interface IDiffResult extends JsDiff.IDiffResult{
+	count:number;
+}
+
+
 export class GutterSvn {
+	
+	
 
 	private timeout: number = null;
 	private decoType: vscode.TextEditorDecorationType[] = [];
+	private ranges: vscode.Range[] = [];
+	private triBottom:string;
+	
+	private headText:string;
 
 	constructor(
 		private context: ExtContext
 	) {
+		
+		this.initProps();
+		
 		if (!!ae) {
-			this.triggerUpdateDecorations(context)
+			//this.triggerUpdateDecorations(context)
+			this.updateDecorations();
 		}
 		
 		
@@ -72,49 +118,108 @@ export class GutterSvn {
 		this.initEvents(context);
 	}
 	
+	initProps(){
+		this.triBottom = this.context.asAbsolutePath(`img/tri_bottom.png`);
+		this.headText = this.getSvnHead();
+		
+		this.decoType[DecoType.MODIFIED] = this.createDecoTypes(DecoType.MODIFIED);
+		this.decoType[DecoType.ADDED] = this.createDecoTypes(DecoType.ADDED);
+		this.decoType[DecoType.DELETED] = this.createDecoTypes(DecoType.DELETED);
+	}
 	
 
 	initEvents(context: ExtContext) {
 		
-		event.subscribeEvent<VSCodeChageEvent>(
+		
+		/* */
+		event.subscribeEvent<VSCodeChangeEvent>(
 			w.onDidChangeActiveTextEditor,
-			this.changeEvent,
+			this.changeEditorFocusEvent.bind(this),
 			context
 		);
-		event.subscribeEvent<VSCodeChageEvent>(
+		event.subscribeEvent<VSCodeChangeEvent>(
 			ws.onDidChangeTextDocument,
-			this.changeEvent,
+			this.changeEvent.bind(this),
 			context
 		);
+		//*/
 		
 		event.subscribeEvent<vscode.TextDocument>(
 			ws.onDidOpenTextDocument,
-			this.fileEvent,
+			this.fileEvent.bind(this),
 			context
 		);
 		event.subscribeEvent<vscode.TextDocument>(
 			ws.onDidSaveTextDocument,
-			this.fileEvent,
+			this.fileEvent.bind(this),
 			context
 		);
 		console.log(`event subscribed`)
 
 	}
+	
+	private changeEditorFocusEvent<T extends VSCodeChangeEvent>(e:T){
+		console.log(`changeEditorEvent called`)
+		ae = w.activeTextEditor;
+		if (!!ae) {
+			console.log(`c target is ${e.document.lineCount}`);
+			//this.triggerUpdateDecorations(this.context);
+			this.headText = this.getSvnHead();
+			this.updateDecorations();
+		}
+		
+	}
 
-	private changeEvent<T extends VSCodeChageEvent>(e: T) {
+	//*
+	private changeEvent<T extends VSCodeChangeEvent>(e: T) {
 		console.log(`changeEvent called`)
 		if (!!ae && e.document === ae.document) {
-			console.log(`target is ${e.document.lineCount}`)
-			this.triggerUpdateDecorations(this.context);
+			console.log(`c target is ${e.document.lineCount}`);
+			//this.triggerUpdateDecorations(this.context);
+			
+			this.updateDecorations();
 		}
 	}
+	/**/
 
 	private fileEvent<T extends vscode.TextDocument>(e: T) {
 		console.log(`fileEvent called`)
 		if (!!ae && e === ae.document) {
-			console.log(`target is ${e.lineCount}`)
-			this.triggerUpdateDecorations(this.context);
+			this.headText = this.getSvnHead();
+			console.log(`f target is ${e.lineCount}`)
+			//this.triggerUpdateDecorations(this.context);
+			this.updateDecorations();
 		}
+	}
+	
+	updateDecorations(){
+		const time = (<any>console).time;
+		const timeEnd = (<any>console).timeEnd;
+		
+		time("new_marked_total");
+		
+		time("compare");
+		let lines = this.compareText(this.headText, ae.document.getText())
+		timeEnd("compare");
+		
+		time("classify");
+		let clines = this.createTempLines(lines)
+			.map(v => this.classifyLine(v))
+		timeEnd("classify");
+		
+		time("deco");
+		const ranges = this.decoType
+			.map((v,i)=>{
+				return this.getRangesByDecoType(clines, i)
+			})
+			.forEach((v,i)=>{
+				ae.setDecorations(
+					this.decoType[i],
+					v
+				);
+			});
+		timeEnd("deco");
+		timeEnd("new_marked_total");
 	}
 
 
@@ -149,16 +254,160 @@ export class GutterSvn {
 			//*/
 	}
 
-/*
-	wrapGetSvnDiff(context: ExtContext) {
-		//return this.getSvnDiff(context)
-		this.getSvnDiff(context)
+	getSvnHead():string{
+		
+		var out:Buffer|string;
+		
+		try {
+			out = child.execSync(
+				`svn cat -r HEAD "${ae.document.fileName}"`,
+				{
+					cwd: ws.rootPath,
+					maxBuffer: 10000000
+				}
+			)
+		} catch (e) {
+			console.log(`svn cat error ${e}`)
+			out = ae.document.getText();
+		}
+		
+		
+		
+		return out.toString();
 	}
-*/
+	
+	compareText(
+		head:string = this.headText,
+		current:string = ae.document.getText()
+	):IParsedDiff{
+		//const s = 
+		// JsDiff.diffLines(head, current, {newlineIsToken: true});
+		//console.log(s);
+		const s = JsDiff.createPatch("difffile", head, current, "old","new");
+		//JsDiff.parsePatch(diffStr)
+		//JSON.stringify
+		return diffParse(s)[0];
+	}
+	
+	createTempLines(parsedDiff:IParsedDiff){
+		let lns:{ln:number,added?:boolean,removed?:boolean,content:string}[] = [];
+		
+		parsedDiff.lines
+			.filter(v => v.type === "add" || v.type === "del")
+			.forEach(v =>{
+				
+				let isAdded:boolean;
+				let isRemoved:boolean;
+				
+				switch(v.type){
+					case "add":
+						isAdded = true;
+						break;
+					case "del":
+						isRemoved = true;
+						break;
+				}
+				try{
+					let t = lns[v.ln];
+					
+					if(!t) t = null;
+					lns[v.ln] = {
+						ln:v.ln,
+						added: !t || !t.added ? isAdded : t.added,
+						removed: !t || !t.removed ? isRemoved : t.removed,
+						content: v.content
+					}
+				}catch(e){
+					console.log(e);
+					Promise.reject(e);
+				}
+			})
+		
+		//console.log(lns)
+		
+		
+		return lns.filter(v=> !!v);
+	}
+	
+	classifyLine(line:TempLine):ClassifiedLine{
+		let decoType:DecoType;
+		
+		if(!!line.added && !!line.removed){
+			decoType = DecoType.MODIFIED;
+		}
+		else if(!!line.added && !line.removed){
+			decoType = DecoType.ADDED;
+		}
+		else if(!line.added && !!line.removed){
+			decoType = DecoType.DELETED;
+		}
+		
+		return {
+			value:line.content,
+			lineNumber: line.ln,
+			decoType
+		}
+	}
+	
+	getRangesByDecoType(lines:ClassifiedLine[], decoType:DecoType):vscode.Range[]{
+		return lines
+			.map((v) =>{
+				if(v.decoType === decoType){
+					let ln = v.lineNumber - 1;
+					return new vscode.Range(ln,0,ln,1);
+				}else{
+					return null;
+				}
+			})
+			.filter(v => v !== null);
+	}
+	
+	
+	
+	createDecoTypes(decoType:DecoType){
+		let col: string = ""
+		let leftWidth: number = 2;
+		let wholeLine: boolean = true;
+		let bg: string = "transparent";
+		
+		switch(decoType){
+			case DecoType.MODIFIED:
+				col = "#080"	//light green
+				break;
+			case DecoType.ADDED:
+				col = "#88f"	//light blue
+				break;
+			case DecoType.DELETED:
+				col = "transparent"
+				leftWidth = 0
+				wholeLine = false;
+				bg = `${bg}; background:transparent url("${this.triBottom}") top left no-repeat !important`
+				break;
+		}
+		
+		const deco: vscode.DecorationRenderOptions = {
+			isWholeLine: wholeLine,
+			overviewRulerLane: vscode.OverviewRulerLane.Left,
+			dark: {
+				overviewRulerColor: col,
+				borderWidth: `0px 0px 0px ${leftWidth}px`,
+				borderStyle: "solid",
+				borderColor: col,
+				backgroundColor: `${bg}`,
+			}
+		}
+		
+		return w.createTextEditorDecorationType(deco);
+	}
+	
+	///////older methods
 
 	getSvnDiff(context: ExtContext) {
 		console.log(`getSvnDiff()`)
-		const tPath = vscode.window.activeTextEditor.document.fileName
+		const tPath = ae.document.fileName
+		
+		
+		
 
 		if (!tPath || tPath === "/1") return;
 		else { console.info(tPath) }
@@ -177,6 +426,13 @@ export class GutterSvn {
 			)
 			.catch(e => ui.error(e))
 		*/
+		//const tPaths = tPath.split("/");
+		const txt = ae.document.getText();
+		/*const tmpPath = `${tPath}.tmp`;
+		fs.writeFileSync(
+			tmpPath,
+			txt
+		)*/
 		
 		/** */
 		child.exec(
@@ -193,7 +449,7 @@ export class GutterSvn {
 		)//*/
 	}
 	
-	
+	/*
 	parse(stdout:Buffer):MarkedLine[]{
 		return diffParse(stdout)
 		.map((file:{
@@ -221,7 +477,7 @@ export class GutterSvn {
 			
 			return markLine;
 		})
-	}
+	}*/
 	
 	/*
 	mark(markLine:MarkedLine[]){
@@ -246,7 +502,7 @@ export class GutterSvn {
 	parseAll(stdout: Buffer) {
 		console.log("parseAll()")
 		diffParse(stdout)
-			.forEach((file: { lines: { type: string, ln: number, content: string }[] }) => {
+			.forEach((file: IParsedDiff) => {
 				let markLine: { add: boolean, del: boolean, contentLength?: number }[] = []
 			
 				//console.log(`get buffer`)
@@ -302,8 +558,6 @@ export class GutterSvn {
 		del: boolean,
 		context: ExtContext
 	) {
-
-		const ae = w.activeTextEditor;
 		const pos = new vscode.Position(lines, charaNum)
 
 		const end = ae
@@ -314,6 +568,7 @@ export class GutterSvn {
 		const decoRange = new vscode
 			.Range(lines, 0, lines, end);
 		
+		//this.ranges[lines] = decoRange;
 		/*	
 		console.log(
 			`ae:${ae.document.fileName}`,
@@ -332,7 +587,6 @@ export class GutterSvn {
 		//ae.setDecorations()
 		ae.setDecorations(
 			this.decoType[lines],
-			//teDeco,
 			[decoRange]
 		);
 	}
@@ -362,12 +616,12 @@ export class GutterSvn {
 		}
 
 		//const triTop = context.asAbsolutePath(`img/tri_top.png`)
-		const triBottom = context.asAbsolutePath(`img/tri_bottom.png`)
+		
 
 		let bg: string = "transparent";
 		if (!add && del) {
 			//console.log(`del line:${lines}`)
-			bg = `${bg}; background:transparent url("${triBottom}") top left no-repeat !important`
+			bg = `${bg}; background:transparent url("${this.triBottom}") top left no-repeat !important`
 			//console.log(`bgColor: ${bg}`)
 		}
 
